242609d0cbe25e27535056c1bc85e890faff38ab
amfl <amfl@none.none>
2017-10-22 20:37:53 +1300

    Small changes

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,29 @@
 extern crate pancurses;
 
 fn main() {
-    println!("Hello, world!");
-
     let win = pancurses::initscr();
 
     let mut running = true;
 
+    let screen_width = 30;
+    let screen_height = 15;
+
+    let mut player_x = screen_width / 2;
+    let mut player_y = screen_height / 2;
+
     while running {
+        win.mvprintw(player_x, player_y, "@");
+        win.refresh();
+
         let input = win.getch();
+        win.mvprintw(player_x, player_y, " ");
+
         match input {
             Some(pancurses::Input::Character('q')) => { running = false; },
             Some(_) => {},
             None => {},
         }
     }
+
+    pancurses::endwin();
 }
