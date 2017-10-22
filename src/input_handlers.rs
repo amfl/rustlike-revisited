dfea0ebde4ec6503b672c7d4451e8029d98d526a
amfl <amfl@none.none>
2017-10-23 12:15:55 +1300

    Simple movement debug text

--- src/input_handlers.rs
+++ src/input_handlers.rs
@@ -1,5 +1,12 @@
 extern crate pancurses;
 
 pub fn handle_keys(user_input: pancurses::Input) {
-    info!("Pushed key")
+    info!("Pushed key: {:?}", user_input);
+    match user_input {
+        pancurses::Input::Character('h') => { info!("left") },
+        pancurses::Input::Character('j') => { info!("down") },
+        pancurses::Input::Character('k') => { info!("up") },
+        pancurses::Input::Character('l') => { info!("right") },
+        _ => {}
+    }
 }
